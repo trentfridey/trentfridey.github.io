ad2d403cb37270233f20b49fd5a0aa92533239ad
Glitch (trentfridey-portfolio) <none>
2017-12-19 06:11:49 +0000

    😘🍮 Updated with Glitch

---
+++ client.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded",function(){
+  
+  var icon = document.getElementById('icon');
+  var nav = document.getElementById('mnav');
+  function toggleNav(){
+    
+    if(nav.classList.contains('mnav-active')){
+      console.log('it works')
+      nav.classList.remove('mnav-active');
+      nav.classList.add('mnav-inactive');
+    }
+    else if(nav.classList.contains('mnav-inactive')){
+      console.log('still working')
+      nav.classList.remove('mnav-inactive');
+      nav.classList.add('mnav-active');
+    }
+  }
+  icon.addEventListener('click',function(){
+    toggleNav();
+  })
+})
