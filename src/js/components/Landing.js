5a504d734575808405f783d6d98fb92ef6f63089
trentfridey <tfri.92@gmail.com>
2020-05-12 21:05:34 -0500

    add links to landing section

--- src/js/components/Landing.js
+++ src/js/components/Landing.js
@@ -4,6 +4,11 @@ const Landing = () => (
     <main>
         <div id="block"></div>
         <p>Trent<br/>Fridey<span id="greeting">Portfolio</span></p>
+        <div id="landing-links">
+            <a href="https://github.com/trentfridey">Github</a>{' - '}
+            <a href="https://www.linkedin.com/in/trentfridey/">LinkedIn</a>{' - '}
+            <a href="#contact">Contact Me!</a>
+        </div>
     </main>
 )
 
